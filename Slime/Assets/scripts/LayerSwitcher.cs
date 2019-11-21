d76239564ebf468a0165407626e5adaf397394ca
MarianaRose <57778170+MarianaRose@users.noreply.github.com>
2019-11-21 10:02:02 -0800

    I've added a scene, and worked some on switching layers, but not currently functuional

---
+++ Slime/Assets/scripts/LayerSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerSwitcher : MonoBehaviour
+{
+    public GameObject slime;
+    public GameObject frontLayer;
+    public GameObject backLayer;
+    public GameObject midLayer;
+    public int startingLayer;
+    private int startLayRecord;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startLayRecord = startingLayer;
+        if (Input.GetKeyDown(KeyCode.DownArrow) && startingLayer > 1)
+        {
+            startingLayer = startingLayer - 1; 
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && startingLayer < 3)
+        {
+            startingLayer = startingLayer + 1;
+        }
+        if (startingLayer != startLayRecord)
+        {
+            if (startingLayer = 1)
+            {
+                slime.layer = "back"
+            }
+        }
+    }
+}
---
+++ Slime/Assets/scripts/mover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour
+{
+    public string layer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer temp;
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            temp = transform.GetChild(i).gameObject.GetComponent<SpriteRenderer>();
+            if ( temp != null)
+            {
+                temp.sortingLayerName = layer;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
